dc0239076b0441a12b43e4974b8e77704817de40
Vinod Kumar <webindianlion@gmail.com>
2021-09-06 08:38:09 +0530

    local json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,27 @@ export class AppComponent implements OnInit{
  
   allProducts:any;
   allCategory:any = [];
+  allProduct_Type:any = [];
+  allProductTypeData:any;
+  noData:boolean=true;
 
   constructor(private httpservice:MakeupService) {}
 
   ngOnInit(){
     this.allProductsData();
     // this.allProductsCategory();
+    this.allProductsType(); 
   }
     
   allProductsData(){
     this.httpservice.getAllProducts().subscribe((data) => {
       this.allProducts = data;
-      // console.log(this.allProducts);
+      this.allProducts = this.allProducts;
+      if(this.allProducts.length > 0) {
+          this.noData= false;
+          this.allProductsType(); 
+      }
+      // console.log(this.allProducts.length);
     });
   }
   allProductsCategory(){
@@ -35,7 +44,42 @@ export class AppComponent implements OnInit{
 
       this.allCategory = this.allCategory.filter((x:any) => x != null);
       
-    console.log(this.allCategory);
+    // console.log(this.allCategory);
   }
 
+  allProductsType(){
+
+    for(var i=0; i < this.allProducts.length; i++){
+      this.allProduct_Type.push(this.allProducts[i].product_type);
+    }
+    this.allProduct_Type = [...new Set(this.allProduct_Type)];
+    // console.log(this.allProduct_Type);
+  }
+
+  productsByType(event:any) {
+    this.allProducts = [];
+    this.noData= true;
+    console.log(event.target.value);
+    this.httpservice.product_type = event.target.value;
+    this.httpservice.getProductsByType().subscribe((data) => {
+      this.allProductTypeData = data;
+      this.allProductTypeData = this.allProductTypeData;
+      this.allProducts = this.allProductTypeData;
+
+      // this.allProducts = this.allProducts.sort((a:any, b:any) => (a.price > b.price) ? 1 : (a.price === b.price) ? ((a.name > b.name) ? 1 : -1) : -1 );
+
+      this.noData= false;
+      // console.log(this.allProductTypeData);
+    });
+  }
+
+  sortByPrice() {
+    // this.allProducts = this.allProducts.sort((a:any, b:any) => (a.price > b.price) ? 1 : (a.price === b.price) ? ((a.name > b.name) ? 1 : -1) : -1 );
+
+    this.allProducts = this.allProducts.sort((a:any, b:any) => {return a.price - b.price;})
+  }
+
+  sortByPriceReverse() {
+    // this.allProducts.reverse();
+  }
 }
--- src/app/makeup.service.ts
+++ src/app/makeup.service.ts
@@ -6,10 +6,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MakeupService {
 
-  urlAllProducts:any = "http://makeup-api.herokuapp.com/api/v1/products.json";
+  product_type:any;
+  urlAllProducts:any = "assets/products.json";
   constructor( private http:HttpClient) { }
 
   getAllProducts(){
     return this.http.get(this.urlAllProducts);
   }
+
+  getProductsByType(){
+    return this.http.get(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${this.product_type}`);
+  }
+  
 }
